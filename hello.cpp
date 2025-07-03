ad6aae5a3d5802ac5f1295a4f29a508189688c99
MehakSharma14 <mehaksharma200614@gmail.com>
2025-07-03 15:35:05 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,19 @@
-// Even and Odd
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int num;
-//     cout<<"enter number:";
-//     cin>>num;
-//     if(num%2==0){
-//         cout<<"even";
-//     }
-//     else{
-//         cout<<"odd";
-//     }
+//Even and Odd
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter number:";
+    cin>>num;
+    if(num%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 // prime number
 #include<iostream>
