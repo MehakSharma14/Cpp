0e8d34dfb1c9cb62f768f775570a070365806e1f
Mehak <mehaksharma200614@gmail.com>
2025-07-04 15:22:54 +0530

    initial commit

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,26 @@
 // Even and Odd
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int num;
-//     cout<<"enter number:";
-//     cin>>num;
-//     if(num%2==0){
-//         cout<<"even";
-//     }
-//     else{
-//         cout<<"odd";
-//     }
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter number:";
+    cin>>num;
+    if(num%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 // prime number
 #include<iostream>
 using namespace std;
 int main(){
     int n;
-    cout<<"enter number";
+    cout<<"enter number: ";
     cin>>n;
     int a =0;
     for(int i=2;i<=n-1;i++){
---
+++ hello_1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter a: "<<endl;
+    cin>>a;
+    cout<<"enter b: "<<endl;
+    cin>>b;
+    cout<<"press 1 for addittion"<<endl<<"press 2 for addittion"<<endl<<"press 3 for addittion"<<endl<<"press 4 for addittion";
+    int ch;
+    // cin>>a;
+    // cin>>b;
+    cin>>ch;
+    switch(ch){
+        case 1:
+        cout<<"result: "<< a+b;
+        break;
+        case 2:
+        cout<<"result: "<< a-b;
+        break;
+        case 3:
+        cout<<"result: "<< a*b;
+        break;
+        case 4:
+        cout<<"result: "<< a/b;
+        break;
+        default:
+        cout<<"error";
+        break;
+    }
+    return 0;
+}
