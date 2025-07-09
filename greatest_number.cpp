fa14490e6bb3a86f383f0fefc56c4dbbf06e288a
Mehak <mehaksharma200614@gmail.com>
2025-07-09 14:34:41 +0530

    initial commit

---
+++ greatest_number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter a: ";
+    cin>>a;
+    cout<<"enter b: ";
+    cin>>b;
+    cout<<"enter c: ";
+    cin>>c;
+    if(a>b && a>c){
+        cout<<"greatest number is:"<<a;
+    }
+    else if(b>a && b>c){
+        cout<<"greatest number is:"<<b;
+    }
+    else{
+        cout<<"greatest number is:"<<c;
+    }
+}
--- hello.cpp
+++ hello.cpp
@@ -1,43 +1,73 @@
-<<<<<<< HEAD
-// Even and Odd
-=======
-//Even and Odd
->>>>>>> ad6aae5a3d5802ac5f1295a4f29a508189688c99
+// // Even and Odd
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     cout<<"enter number:";
+//     cin>>num;
+//     if(num%2==0){
+//         cout<<"even";
+//     }
+//     else{
+//         cout<<"odd";
+//     }
+
+//     return 0;
+// }
+
+// // prime number
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter number: ";
+//     cin>>n;
+//     int a =0;
+//     for(int i=2;i<=n-1;i++){
+//         if(n%i==0){
+//             a=1;
+//             break;
+//         }
+//     }
+//     if(a==0){
+//         cout<<"prime";
+//     }
+//     else{
+//         cout<<"odd";
+//     }
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int num;
-    cout<<"enter number:";
-    cin>>num;
-    if(num%2==0){
-        cout<<"even";
-    }
-    else{
-        cout<<"odd";
-    }
-
-    return 0;
-}
-
-// prime number
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"enter number: ";
-    cin>>n;
-    int a =0;
-    for(int i=2;i<=n-1;i++){
-        if(n%i==0){
-            a=1;
-            break;
-        }
-    }
-    if(a==0){
-        cout<<"prime";
-    }
-    else{
-        cout<<"odd";
+    int a,b;
+    cout<<"enter a: "<<endl;
+    cin>>a;
+    cout<<"enter b: "<<endl;
+    cin>>b;
+    cout<<"press 1 for addittion"<<endl<<"press 2 for subtraction"<<endl<<"press 3 for multiply"<<endl<<"press 4 for division";
+    int ch;
+    // cin>>a;
+    // cin>>b;
+    cin>>ch;
+    switch(ch){
+        case 1:
+        cout<<"result: "<< a+b;
+        break;
+        case 2:
+        cout<<"result: "<< a-b;
+        break;
+        case 3:
+        cout<<"result: "<< a*b;
+        break;
+        case 4:
+        cout<<"result: "<< a/b;
+        break;
+        default:
+        cout<<"error";
+        break;
     }
     return 0;
 }
