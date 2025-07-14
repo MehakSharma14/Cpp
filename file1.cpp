691d91ed28c3103f26ba6344a1b89624f57f8d80
MehakSharma14 <mehaksharma200614@gmail.com>
2025-07-14 11:49:24 +0530

    Add files via upload

---
+++ file1.cpp
@@ -0,0 +1,242 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements: ";
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    
+    int max = arr[0], min = arr[0];
+    for (int i=0; i<n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    for (int i=0; i<n; i++) {
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    
+    cout << "Maximum: " << max << endl << "Minimum: " << min;
+}
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     int arr[n];
+//     cout << "Enter elements: ";
+//     for(int i=0; i<n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     cout << "Reversed array: ";
+    
+//     for(int i=n-1; i>=0; i--) {
+//         cout << arr[i] << " ";
+//     }  
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number: ";
+//     cin >> n;
+    
+//     cout << "Enter the elements: ";
+//     int arr[n];
+//     for(int i=0; i<n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     for(int i=0; i<n-1; i++) {
+//         for(int j=0; j<n-i-1; j++) {
+//             if(arr[j] > arr[j+1]) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1]; 
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+    
+//     for(int i=0; i<n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     cout << arr[n-2];
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     int arr[n], new_arr[n];
+    
+//     cout << "Enter elements: ";
+//     for(int i=0; i<n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     for (int i=0; i<n; i++) {
+//         new_arr[i] = arr[i];
+//     }
+    
+//     for (int i=0; i<n-1; i++) {
+//         for (int j=0; j<n-i-1; j++) {
+//             if (arr[j] > arr[j+1]) {
+//                 int temp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1] = temp;
+//             }
+//         }
+//     }
+    
+//     bool sorted = true;
+//     for (int i=1; i<n; i++) {
+//         if(arr[i] != new_arr[i]) {
+//             sorted = false;
+//             break;
+//         }
+//     }
+    
+//     if (sorted) {
+//         cout << "Sorted";
+//     } else {
+//         cout << "Not Sorted";
+//     }
+    
+    
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     int arr[n], new_arr[n];
+    
+//     cout << "Enter elements: ";
+//     for(int i=0; i<n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     int x;
+//     cout << "Enter the element to count frequency: ";
+//     cin >> x;
+//     int freq = 0;
+//     for (int i=0; i<n; i++) {
+//         if(arr[i] == x) {
+//             freq++;
+//         }
+//     }
+    
+//     cout << "Frequency: " << freq;
+    
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+//     int arr[n];
+    
+//     cout << "Enter elements: ";
+//     for(int i=0; i<n; i++) {
+//         cin >> arr[i];
+//     }
+    
+//     int sum;
+//     cout << "Enter the sum: ";
+//     cin >> sum;
+    
+//     for(int i=0; i<n; i++) {
+//         for(int j=i+1; j<n; j++) {
+//             if(arr[i] + arr[j] == sum) {
+//                 cout << arr[i] << "," << arr[j];
+//             }
+//         }
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
